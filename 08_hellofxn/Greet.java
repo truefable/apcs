364198433366cfe3a30c85e4817218454ba74fea
truefable <90898208+truefable@users.noreply.github.com>
2021-10-05 14:03:14 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,9 +1,16 @@
+//Eric He
+//APCS
+//HW08 -- Refactor Freshie Zero
+//2021-10-01
+
 public class Greet{
 	public static void main(String[] args){
-		greet(String);
+		greet("p1");
+		greet("p2");
+		greet("p3");
 	}
 
-	public static void greet(String){
-		System.out.println(String);
+	public static void greet(String name){
+	System.out.println("Why, hello there, " + name + ". How do you do?");
 	}
 }
