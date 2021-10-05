a2302c699758541700b726c23a7ab3425b06d2f5
truefable <90898208+truefable@users.noreply.github.com>
2021-10-05 14:04:37 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -11,6 +11,6 @@ public class Greet{
 	}
 
 	public static void greet(String name){
-	System.out.println("Why, hello there, " + name + ". How do you do?");
+		System.out.println("Why, hello there, " + name + ". How do you do?");
 	}
 }
