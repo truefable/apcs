4243867383856ed4ef1f308252e45ba78c5ea435
truefable <90898208+truefable@users.noreply.github.com>
2021-10-05 13:55:56 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,5 +1,10 @@
+//Eric He
+//APCS
+//HW09 -- What are BigSibs Good For?
+//2021-10-04
+
 public class BigSib{
-	public static void greet(String){
-		System.out.println(String);
+	public static void greet(String name){
+		System.out.println("Why, hello there, " + name + ". How do you do?");
 	}
 }
