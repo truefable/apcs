c3d419e15920021b6cfd01c0f52f17d4285d7721
truefable <90898208+truefable@users.noreply.github.com>
2021-10-07 13:31:59 -0400

    Update Greet.java

--- 13_bigsib/Greet.java
+++ 13_bigsib/Greet.java
@@ -1,1 +1,28 @@
 //Eric
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+	
+	BigSib joe = new BigSib("Salutations");
+	
+	greeting= joe.greet("Dr. Spaceman");
+	System.out.println(greeting);
+	
+	BigSib mama = new BigSib("Hey ya");
+	
+	greeting= mama.greet("Kong Fooey");
+	System.out.println(greeting);
+	
+	BigSib sky = new BigSib("sup");
+	
+	greeting= sky.greet("Mom");
+	System.out.println(greeting);
+
+  }
+}
