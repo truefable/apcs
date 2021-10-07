8388edf5e3147027b5ab3121fb233266535f839a
truefable <90898208+truefable@users.noreply.github.com>
2021-10-07 13:30:50 -0400

    Update BigSib.java

--- 13_bigsib/BigSib.java
+++ 13_bigsib/BigSib.java
@@ -1,1 +1,23 @@
 //Eric He
+//APCS
+//HW13 -- Where do BigSibs Come From?
+//2021-10-07
+
+/* DISCO: the constructor has the name of the class
+QCC:
+*/
+  
+public class BigSib {
+	String helloMsg;
+	public BigSib() {
+		helloMsg = "Hello";
+	}
+	public String setHelloMsg(String s) {
+		helloMsg = s;
+		return helloMsg;
+	}
+	public String greet(String s) {
+		String message = (helloMsg + " " + s);
+		return message;
+	}
+}
