02dfb426384da73e8c398387e1343ef0233804be
truefable <90898208+truefable@users.noreply.github.com>
2021-10-19 14:05:53 -0400

    Update Teller.java

--- 20/Teller.java
+++ 20/Teller.java
@@ -1,5 +1,6 @@
 public class Teller{
 	public void main(String args[]){
+		//Test
 		BankAccount ba = new BankAccount();
     		ba.setName("A");
     		ba.setPassword("pass123");
@@ -11,5 +12,24 @@ public class Teller{
     		System.out.println(ba.toString());
     		ba.withdraw(2000.90);
     		System.out.println(ba.toString());
+		
+		//Break
+		BankAccount p1 = new BankAccount();
+		p1.setName("p1");
+		p1.setPin(999);
+		System.out.println(p1.toString());
+		
+		BankAccount p2 = new BankAccount();
+		p2.setAccountNumber(1111111111);
+		System.out.println(p2.toString());
+		
+		BankAccount p3 = new BankAccount();
+		p3.setBalance(-100);
+		System.out.println(p3.toString());
+		
+		BankAccount p4 = new BankAccount();
+		p4.setBalance(100);
+		p4.withdraw(-100);
+		System.out.println(p4.toString());
     }		
 }
