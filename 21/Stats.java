be946efabc8849dc9ba841a974cd63b8b7080b56
truefable <90898208+truefable@users.noreply.github.com>
2021-10-20 13:24:47 -0400

    Update Stats.java

--- 21/Stats.java
+++ 21/Stats.java
@@ -13,23 +13,28 @@
 public class Stats {
 
   public static int mean(int a, int b) {
-
+     int avg = (1/2) * (a + b);
+     return avg;
   }
 
   public static double mean(double a, double b) {
-
+     double avg = (1/2) * (a + b);
+     return avg;
   }
 
   public static int max(int a, int b) {
-
+     int m = (1/2) * (a + b) + (1/2) * Math.abs(a - b);
+     return m;
   }
 
   public static double max(double a, double b) {
-
+     double m = (1/2) * (a + b) + (1/2) * Math.abs(a - b);
+     return m;
   }
 
   public static int geoMean(int a, int b) {
-
+     int gm = Math.sqrt(a * b);
+     return gm;
   }
 
   public static double geoMean(double a, double b) {
