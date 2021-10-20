4184b6c248effd94082f246adcd6d897be5c0f90
truefable <90898208+truefable@users.noreply.github.com>
2021-10-20 13:16:27 -0400

    Add files via upload

---
+++ 21/StatsDriver.java
@@ -0,0 +1,13 @@
+public class StatsDriver{
+	public static void main(String args[]){
+		Stats.mean(1,3);//return 2
+		Stats.mean(1,2);//return 1
+		Stats.mean(0.12,0.34);//return 0.23
+		Stats.max(1,2);//return 2
+		Stats.max(1.2,2.1);//return 2.1
+		Stats.geoMean(2,2);//return 2
+		Stats.geoMean(2,-2);//return error
+		Stats.geoMean(-2,-2);//return 2
+		Stats.geoMean(1.2,-2.1);//return error
+	}
+}
