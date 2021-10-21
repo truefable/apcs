06fbab23e62e6ad72e2ea2298e01cdda6c07f889
truefable <90898208+truefable@users.noreply.github.com>
2021-10-20 23:50:14 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -11,103 +11,116 @@
 
 public class Stats {
 
-  public static int mean(int a, int b) {
-     int avg = (1/2) * (a + b);
-     return avg;
-  }
+    public static int mean(int a, int b) {
+        int avg = (a + b) / 2;
+        return avg;
+    }
+  
+    public static double mean(double a, double b) {
+        double avg = (a + b) / 2;
+        return avg;
+    }
+  
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }else return b;
+    }
+  
+    public static double max(double a, double b) {
+        if (a > b) {
+            return a;
+        }else return b;
+    }
+  
+    public static int geoMean(int a, int b) {
+        int product = (a * b);
+        int gm = -1;
+        
+        for(int i = 0; i < product; i++){
+            if ( (i * i) > product) {
+                break;
+            }
+            gm = i;
+        }
+        if (product == 0){
+            gm = 0;
+        }else {
+            if ( (product - ( (gm - 1) * (gm - 1))) < ( (gm * gm) - product) ){
+            gm = gm - 1;
+            }
+        }
+        if (gm < 0){
+            gm = -999;
+        }
+        return gm;
+    }
+  
+    public static double geoMean(double a, double b) {
+        double gm = Math.sqrt(a * b);
+        return gm;
+    }
+  
+    public static int max(int a, int b, int c) {
+        int maxNum = 0;
+        if ((a >= b) && (a >= c)) {
+            maxNum = a;
+        }
+        if ((b >= a) && (b >= c)) {
+            maxNum = b;
+        }
+        if ((c >= a) && (c >= b)) {
+            maxNum = c;
+        }
+        return maxNum;
+    }
+  
+    public static double max(double a, double b, double c) {
+        double maxNum = 0;
+        if ((a >= b) && (a >= c)) {
+            maxNum = a;
+        }
+        if ((b >= a) && (b >= c)) {
+            maxNum = b;
+        }
+        if ((c >= a) && (c >= b)) {
+            maxNum = c;
+        }
+        return maxNum;
+    }
+  
+    public static int geoMean(int a, int b, int c) {
+        int product = (a * b * c);
+        int gm = -1;
+        
+        for(int i = 0; i < product; i++){
+            if ( (i * i * i) > product) {
+                break;
+            }
+            gm = i;
+        }
+        if ( (product - ( (gm - 1) * (gm - 1))) < ( (gm * gm) - product) ){
+            gm = gm - 1;
+        }
+        if (gm < 0){
+            gm = -999;
+        }
+        return gm;
+    }
 
-  public static double mean(double a, double b) {
-     double avg = (1/2) * (a + b);
-     return avg;
-  }
+    public static double geoMean(double a, double b, double c) {
+        double product = a * b * c;
+        double cbrt = 1.0 / 3.0;
 
-  public static int max(int a, int b) {
-     int m = (1/2) * (a + b) + (1/2) * Math.abs(a - b);
-     return m;
-  }
-
-  public static double max(double a, double b) {
-     double m = (1/2) * (a + b) + (1/2) * Math.abs(a - b);
-     return m;
-  }
-
-  public static int geoMean(int a, int b) {
-     
-  }
-
-  public static double geoMean(double a, double b) {
-	  double gm = Math.sqrt(a * b);
-	  return gm;
-  }
-
-  public static int max(int a, int b, int c) {
-	  if (a = b) {
-		  if (a = c) {
-			  return a;
-		  } else if (a > c) {
-			  return a;
-		  } else {
-			  return c;
-		  }
-	  } else if (a > b) {
-		  if (a = c) {
-			  return a;
-		  } else if (a > c) {
-			  return a;
-		  } else {
-			  return c;
-		  }
-	  } else {
-		  if (b = c) {
-			  return b;
-		  } else if (b > c) {
-			  return b;
-		  } else {
-			  return c;
-		  }
-	  }
-  }
-
-  public static double max(double a, double b, double c) {
-	  if (a = b) {
-		  if (a = c) {
-			  return a;
-		  } else if (a > c) {
-			  return a;
-		  } else {
-			  return c;
-		  }
-	  } else if (a > b) {
-		  if (a = c) {
-			  return a;
-		  } else if (a > c) {
-			  return a;
-		  } else {
-			  return c;
-		  }
-	  } else {
-		  if (b = c) {
-			  return b;
-		  } else if (b > c) {
-			  return b;
-		  } else {
-			  return c;
-		  }
-	  }
-  }
-
-  public static int geoMean(int a, int b, int c) {
-  }
-
-  public static double geoMean(double a, double b, double c) {
-	  gm = Math.cbrt(a * b * c);
-	  return gm;
-  }
-
-
-  //main method for testing functionality
-  public static void main( String[] args ) {
-
-  }
-
-}//end class
+        System.out.println(product);
+        double gm = Math.pow(product, cbrt);
+        return gm;
+    }
+  
+  
+    //main method for testing functionality
+    public static void main( String[] args ) {
+  
+    }
+  
+  }//end class
