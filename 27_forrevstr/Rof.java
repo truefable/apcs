8afe8f265c3e03afe0bddd1c6eea646d794a561e
truefable <90898208+truefable@users.noreply.github.com>
2021-10-28 23:04:17 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,28 +1,46 @@
+/*
+Emily Ortiz + Applesauce, Tasnim Chowdhury + Mary, Eric He
+APCS
+HW27 -- FOR the Love of Strings
+2021-10-28
+time spent: .5 hours
+DISCO
+- counter maintenance built in for loop but not in while
+- to get one character in a string in the xth position, do "string".substring(x, x + 1)
+QCC
+- When is a for loop and while loop appropriate?
+*/
+
 public class Rof{
 	public static void main(String args[]){
+		System.out.println("\nfenceF Tests:");
 		System.out.println(fenceF(0));
 		System.out.println(fenceF(1));
 		System.out.println(fenceF(2));
 		System.out.println(fenceF(4));
 		System.out.println(fenceF(8));
-		System.out.println(reverseF("desserts"));
-		System.out.println(reverseF("stressed"));
-		System.out.println(reverseR("desserts"));
-		System.out.println(reverseR("stressed"));
+		
+		System.out.println("\nreverseF Tests:");
+		System.out.println("Should be stressed: " + reverseF("desserts"));
+		System.out.println("Should be desserts: " + reverseF("stressed"));
+		
+		System.out.println("\nreverseR Tests:"); 
+		System.out.println("Should be stressed: " + reverseR("desserts")); 
+		System.out.println("Should be desserts: " + reverseR("stressed"));
 	}
 	
 	public static String fenceF(int n){
-		String f = "l";
+		String f = "|";
 		for (int i = 1; i < n; i++){
-			f = f + "--l";
+			f = f + "--|";
 		}
 		return f;
 	}
 	
 	public static String reverseF(String s){
 		String r = "";
-		for (int i = s.length(); i > 0; i--){
-			r = r + s.substring(i - 1, i - 1);
+		for (int i = s.length() - 1; i >= 0; i--){
+			r = r + s.substring(i, i + 1);
 		}
 		return r;
 	}
@@ -31,7 +49,7 @@ public class Rof{
 		if (s.length() < 2){
 			return s;
 		} else {
-			return reverseR(s.substring(1)) + s.substring(0,0);
+			return reverseR(s.substring(1)) + s.substring(0, 0 + 1);
 		}
 	}
 }
